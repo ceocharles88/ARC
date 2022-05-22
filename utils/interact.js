363d92ab3d0794577f687547b900fa33f33fc1fb
ceocharles88 <leecharles88@gmail.com>
2022-05-22 15:07:10 +0900

    Init

---
+++ utils/interact.js
@@ -0,0 +1,110 @@
+
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3("https://eth-rinkeby.alchemyapi.io/v2/deSJ7VyGLswml7dz5tzzNbHBeYu_R_S1");
+
+const contractABI = require("../pages/contract-abi.json");
+const contractAddress = "0xA13aB596A2833261040290F6d05968aa95946bA2";
+
+const nftContract = new web3.eth.Contract(
+    contractABI,
+    contractAddress
+);
+
+export const connectWallet = async () => {
+
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            const obj = {
+                status: "👆🏽 Write a message in the text-field above.",
+                address: addressArray[0],
+            };
+            return obj;
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
+                    <p>
+                        {" "}
+                        🦊{" "}
+                        <a target="_blank" href={`https://metamask.io/download.html`}>
+                            You must install Metamask, a virtual Ethereum wallet, in your
+                            browser.
+                        </a>
+                    </p>
+                </span>
+            ),
+        };
+    }
+};
+
+export const getCurrentWalletConnected = async () => {
+    if (window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+            if (addressArray.length > 0) {
+                return {
+                    address: addressArray[0],
+                    status: "👆🏽 Write a message in the text-field above.",
+                };
+            } else {
+                return {
+                    address: "",
+                    status: "🦊 Connect to Metamask using the top right button.",
+                };
+            }
+        } catch (err) {
+            return {
+                address: "",
+                status: "😥 " + err.message,
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
+                    <p>
+                        {" "}
+                        🦊{" "}
+                        <a target="_blank" href={`https://metamask.io/download.html`}>
+                            You must install Metamask, a virtual Ethereum wallet, in your
+                            browser.
+                        </a>
+                    </p>
+                </span>
+            ),
+        };
+    }
+
+
+
+
+};
+
+export const getNFTPrice = async () => {
+    const mintPrice = await nftContract.methods.price().call()
+    const priceEther = web3.utils.fromWei(mintPrice, "ether");
+    return priceEther
+}
+
+export const getTotalMinted = async () => {
+    const totalMinted = await nftContract.methods.totalSupply().call()
+    return totalMinted
+}
+
+
+
+
